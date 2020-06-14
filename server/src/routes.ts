8be07c8afb7f90751c5bb0ea6d34618cdc12a809
Luis Silva <luis.silva@wafx.global>
2020-06-13 21:03:02 -0300

    server web

---
+++ server/src/routes.ts
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points', pointsController.index);
+
+routes.post('/points', pointsController.create);
+
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
+
